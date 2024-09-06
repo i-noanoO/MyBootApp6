622b44c3edfa6af217a15ccd3a3fc8388ae67aab
i-noanoO <te210584@gmail.com>
2024-09-06 17:20:38 +0900

    修正

--- src/main/java/jp/te4a/spring/boot/myapp7/mybootapp7/BookBean.java
+++ src/main/java/jp/te4a/spring/boot/myapp7/mybootapp7/BookBean.java
@@ -1,13 +1,12 @@
 package jp.te4a.spring.boot.myapp7.mybootapp7;
-
+ 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
-@Data
+ 
+@Data//getter・setter自動生成
 @AllArgsConstructor
-
-public class BookBean{
-    Integer id;
+public class BookBean {
+    Integer id ;
     String title;
     String writter;
     String publisher;
--- src/main/java/jp/te4a/spring/boot/myapp7/mybootapp7/Mybootapp7Application.java
+++ src/main/java/jp/te4a/spring/boot/myapp7/mybootapp7/Mybootapp7Application.java
@@ -1,13 +1,13 @@
-package jp.te4a.spring.boot.myapp7.mybootapp7;
-
+package jp.te4a.spring;
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
-public class Mybootapp7Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Mybootapp7Application.class, args);
-	}
-
+public class MyBootApp7Application {
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(MyBootApp7Application.class, args);
+    }
+ 
 }
